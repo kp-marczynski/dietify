65f98a69258a7062a34d535d896e2c51877f29a1
kp-marczynski <kp.marczynski@gmail.com>
2019-03-18 19:14:49 +0100

    added roles for dietician and patient

--- src/main/java/pl/marczynski/dietify/core/security/AuthoritiesConstants.java
+++ src/main/java/pl/marczynski/dietify/core/security/AuthoritiesConstants.java
@@ -9,6 +9,10 @@ public final class AuthoritiesConstants {
 
     public static final String USER = "ROLE_USER";
 
+    public static final String DIETICIAN = "ROLE_DIETICIAN";
+
+    public static final String PATIENT = "ROLE_PATIENT";
+
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
     private AuthoritiesConstants() {
