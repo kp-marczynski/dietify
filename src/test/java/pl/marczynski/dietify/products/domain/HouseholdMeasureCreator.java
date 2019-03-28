53dbff06bcfb83215399ca9484d2e054958f503e
kp-marczynski <krzysztof.marczynski@atos.net>
2019-03-28 09:47:00 +0100

    #11 fix

--- src/test/java/pl/marczynski/dietify/products/domain/HouseholdMeasureCreator.java
+++ src/test/java/pl/marczynski/dietify/products/domain/HouseholdMeasureCreator.java
@@ -5,8 +5,8 @@ public class HouseholdMeasureCreator {
     public static final String DEFAULT_DESCRIPTION = "AAAAAAAAAA";
     public static final String UPDATED_DESCRIPTION = "BBBBBBBBBB";
 
-    public static final Double DEFAULT_GRAMS_WEIGHT = 0D;
-    public static final Double UPDATED_GRAMS_WEIGHT = 1D;
+    public static final Double DEFAULT_GRAMS_WEIGHT = 1D;
+    public static final Double UPDATED_GRAMS_WEIGHT = 2D;
 
     public static final Boolean DEFAULT_IS_VISIBLE = false;
     public static final Boolean UPDATED_IS_VISIBLE = true;
