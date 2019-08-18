b13e0b87724240469ce935574d4d306635ab9946
kp-marczynski <kp.marczynski@gmail.com>
2019-08-18 22:59:16 +0200

    #84 patient card detail view foundation

--- gateway/src/main/webapp/app/entities/appointments/patient-card/patient-card-detail.component.ts
+++ gateway/src/main/webapp/app/entities/appointments/patient-card/patient-card-detail.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
-import { IPatientCard } from 'app/shared/model/appointments/patient-card.model';
+import {IPatientCard} from 'app/shared/model/appointments/patient-card.model';
+import {MainLayoutCardService} from 'app/layouts/main/main-layout-card.service';
 
 @Component({
   selector: 'jhi-patient-card-detail',
@@ -10,10 +11,13 @@ import { IPatientCard } from 'app/shared/model/appointments/patient-card.model';
 export class PatientCardDetailComponent implements OnInit {
   patientCard: IPatientCard;
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(protected activatedRoute: ActivatedRoute,
+              protected layoutCardService: MainLayoutCardService) {
+  }
 
   ngOnInit() {
-    this.activatedRoute.data.subscribe(({ patientCard }) => {
+    this.layoutCardService.changeMainCardContainerVisibility(false);
+    this.activatedRoute.data.subscribe(({patientCard}) => {
       this.patientCard = patientCard;
     });
   }
