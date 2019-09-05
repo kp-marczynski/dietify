16e519d625d63f3b74031c3505d766ca7e5b0902
kp-marczysnki <kp.marczynski@gmail.com>
2019-09-05 15:29:52 +0200

    #praca-dyplomowa change output pdf name

--- documentation/latex/compile-tex.sh
+++ documentation/latex/compile-tex.sh
@@ -2,4 +2,4 @@
 # Require package pygments to work:
 # pip install pygments
 # Also in miktex l3backend package might be required to install manually from miktex console.
-pdflatex.exe -synctex=1 -interaction=nonstopmode main.tex --shell-escape
+pdflatex.exe -synctex=1 -interaction=nonstopmode main.tex --shell-escape -job-name=Marczynski_Krzysztof_praca_inzynierska
