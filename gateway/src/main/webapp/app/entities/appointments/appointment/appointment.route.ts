9354c4c9bef4fb196b5e802d6b2cf5561ad7429d
kp-marczynski <kp.marczynski@gmail.com>
2019-10-12 22:48:52 +0200

    #127 handling creation of new appointment

--- gateway/src/main/webapp/app/entities/appointments/appointment/appointment.route.ts
+++ gateway/src/main/webapp/app/entities/appointments/appointment/appointment.route.ts
@@ -1,31 +1,47 @@
-import { Injectable } from '@angular/core';
-import { HttpResponse } from '@angular/common/http';
-import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Routes } from '@angular/router';
-import { JhiPaginationUtil, JhiResolvePagingParams } from 'ng-jhipster';
-import { UserRouteAccessService } from 'app/core';
-import { Observable, of } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-import { Appointment } from 'app/shared/model/appointments/appointment.model';
-import { AppointmentService } from './appointment.service';
-import { AppointmentComponent } from './appointment.component';
-import { AppointmentDetailComponent } from './appointment-detail.component';
-import { AppointmentUpdateComponent } from './appointment-update.component';
-import { AppointmentDeletePopupComponent } from './appointment-delete-dialog.component';
-import { IAppointment } from 'app/shared/model/appointments/appointment.model';
+import {Injectable} from '@angular/core';
+import {HttpResponse} from '@angular/common/http';
+import {Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Routes} from '@angular/router';
+import {JhiPaginationUtil, JhiResolvePagingParams} from 'ng-jhipster';
+import {UserRouteAccessService} from 'app/core';
+import {Observable, of} from 'rxjs';
+import {filter, map} from 'rxjs/operators';
+import {Appointment} from 'app/shared/model/appointments/appointment.model';
+import {AppointmentService} from './appointment.service';
+import {AppointmentComponent} from './appointment.component';
+import {AppointmentDetailComponent} from './appointment-detail.component';
+import {AppointmentUpdateComponent} from './appointment-update.component';
+import {AppointmentDeletePopupComponent} from './appointment-delete-dialog.component';
+import {IAppointment} from 'app/shared/model/appointments/appointment.model';
+import {PatientCardService} from 'app/entities/appointments/patient-card';
+import {PatientCard} from 'app/shared/model/appointments/patient-card.model';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class AppointmentResolve implements Resolve<IAppointment> {
-  constructor(private service: AppointmentService) {}
+  constructor(private service: AppointmentService, private patientCardService: PatientCardService) {
+  }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IAppointment> {
     const id = route.params['id'] ? route.params['id'] : null;
     if (id) {
       return this.service.find(id).pipe(
         filter((response: HttpResponse<Appointment>) => response.ok),
-        map((appointment: HttpResponse<Appointment>) => appointment.body)
+        map((res: HttpResponse<Appointment>) => res.body)
       );
     }
-    return of(new Appointment());
+
+    const patientCardId = route.params['patientCardId'] ? route.params['patientCardId'] : null;
+    const appointment = new Appointment();
+    if (patientCardId) {
+      return this.patientCardService.find(patientCardId).pipe(
+        filter((response: HttpResponse<PatientCard>) => response.ok),
+        map((res: HttpResponse<PatientCard>) => {
+          appointment.patientCard = res.body;
+          return appointment;
+        })
+      );
+
+    }
+    return of(appointment);
   }
 }
 
@@ -57,6 +73,18 @@ export const appointmentRoute: Routes = [
   },
   {
     path: 'new',
+    component: AppointmentUpdateComponent,
+    resolve: {
+      appointment: AppointmentResolve
+    },
+    data: {
+      authorities: ['ROLE_USER'],
+      pageTitle: 'gatewayApp.appointmentsAppointment.home.title'
+    },
+    canActivate: [UserRouteAccessService]
+  },
+  {
+    path: 'new/card/:patientCardId',
     component: AppointmentUpdateComponent,
     resolve: {
       appointment: AppointmentResolve
